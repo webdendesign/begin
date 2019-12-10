af87b516b25bd38a2c8699baaf8f1e5ccd5b7c14
Den <fabri.din@yandex.ru>
2019-12-10 12:33:49 +0500

    done and add map

--- js/map.js
+++ js/map.js
@@ -1,177 +1,93 @@
-var title = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var type = ['palace', 'flat', 'house', 'bungalo'];
+"use strict";
+
+var title = [
+  "Большая уютная квартира",
+  "Маленькая неуютная квартира",
+  "Огромный прекрасный дворец",
+  "Маленький ужасный дворец",
+  "Красивый гостевой домик",
+  "Некрасивый негостеприимный домик",
+  "Уютное бунгало далеко от моря",
+  "Неуютное бунгало по колено в воде"
+];
+var type = ["palace", "flat", "house", "bungalo"];
 var rooms = [1, 2, 3, 4, 5];
-var checkin = ['12:00', '13:00', '14:00'];
-var checkout = ['12:00', '13:00', '14:00'];
-var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var description = '';
-var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var checkin = ["12:00", "13:00", "14:00"];
+var checkout = ["12:00", "13:00", "14:00"];
+var features = [
+  "wifi",
+  "dishwasher",
+  "parking",
+  "washer",
+  "elevator",
+  "conditioner"
+];
+var description = "";
+var photos = [
+  "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
+  "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+];
+
+var randomFeatures = function() {
+  randomElement;
+  return randomElement;
+};
+
 var usersAds = [
- {
-    'author': {'img/avatars/user01.png'},
-
-    'offer': {'title[0]',
-              'address: location.x, location.y',
-              'price[Math.random() * 1000]',
-              'type[0]',
-              'rooms[Math.floor(Math.random() * rooms.length)',
-              'guests[Math.floor(Math.random() * 10)]',
-              'checkin[0]',
-              'checkout[1]',
-              'features[Math.floor(Math.random() * rooms.length)]',
-              'description',
-              'photos[]'},
-
-    'location': {'x = (Math.random() * 500)',
-                 'y = (Math.random() * 500)'}
-
- },
- {
-    'author': {'img/avatars/user02.png'},
-
-    'offer': {'title[1]',
-              'address: location.x, location.y',
-              'price[Math.random() * 1000]',
-              'type[1]',
-              'rooms[Math.floor(Math.random() * rooms.length)]',
-              'guests[Math.floor(Math.random() * 10)]',
-              'checkin[1]',
-              'checkout[2]',
-              'features[Math.floor(Math.random() * rooms.length)]',
-              'description',
-              'photos[]'},
-
-    'location': {'x = Math.floor(Math.random() * 10)',
-                 'y = Math.floor(Math.random() * 10)'}
-  },
   {
-    'author': {'img/avatars/user03.png'},
+    author: {
+      avatar: "img/avatars/user01.png"
+    },
 
-    'offer': {'title[2]',
-              'address: location.x, location.y',
-              'price[Math.random() * 1000]',
-              'type[2]',
-              'rooms[Math.floor(Math.random() * rooms.length)]',
-              'guests[Math.floor(Math.random() * 10)]',
-              'checkin[1]',
-              'checkout[1]',
-              'features[Math.floor(Math.random() * rooms.length)]',
-              'description',
-              'photos[]'},
+    offer: {
+      title: title[0],
+      address: "location.x, location.y",
+      price: Math.floor(Math.random() * 1000000),
+      type: type[0],
+      rooms: Math.floor(Math.random() * rooms.length),
+      guests: Math.floor(Math.random() * 10),
+      checkin: checkin[0],
+      checkout: checkout[1],
+      features: features,
+      description: "",
+      photos: photos
+    },
 
-    'location': {'x = Math.floor(Math.random() * 10)',
-                 'y = Math.floor(Math.random() * 10)'}
-  },
-  {
-    'author': {'img/avatars/user04.png'},
-
-    'offer': {'title[3]',
-              'address: location.x, location.y',
-              'price[Math.random() * 1000]',
-              'type[3]',
-              'rooms[Math.floor(Math.random() * rooms.length)]',
-              'guests[Math.floor(Math.random() * 10)]',
-              'checkin[0]',
-              'checkout[2]',
-              'features[Math.floor(Math.random() * rooms.length)]',
-              'description',
-              'photos[]'},
-
-    'location': {'x = Math.floor(Math.random() * 10)',
-                 'y = Math.floor(Math.random() * 10)'}
-  },
-  {
-    'author': {'img/avatars/user05.png'},
-
-    'offer': {'title[4]',
-              'address: location.x, location.y',
-              'price[Math.random() * 1000]',
-              'type[0]',
-              'rooms[Math.floor(Math.random() * rooms.length)]',
-              'guests[Math.floor(Math.random() * 10)]',
-              'checkin[2]',
-              'checkout[0]',
-              'features[Math.floor(Math.random() * rooms.length)]',
-              'description',
-              'photos[]'},
-
-    'location': {'x = Math.floor(Math.random() * 10)',
-                 'y = Math.floor(Math.random() * 10)'}
-  },
-  {
-    'author': {'img/avatars/user06.png'},
-
-    'offer': {'title[5]',
-              'address: location.x, location.y',
-              'price[Math.random() * 1000]',
-              'type[1]',
-              'rooms[Math.floor(Math.random() * rooms.length)]',
-              'guests[Math.floor(Math.random() * 10)]',
-              'checkin[0]',
-              'checkout[0]',
-              'features[Math.floor(Math.random() * rooms.length)]',
-              'description',
-              'photos[]'},
-
-    'location': {'x = Math.floor(Math.random() * 10)',
-                 'y = Math.floor(Math.random() * 10)'}
-  },
-  {
-    'author': {'img/avatars/user07.png'},
-
-    'offer': {'title[6]',
-              'address: location.x, location.y',
-              'price[Math.random() * 1000]',
-              'type[2]',
-              'rooms[Math.floor(Math.random() * rooms.length)]',
-              'guests[Math.floor(Math.random() * 10)]',
-              'checkin[1]',
-              'checkout[0]',
-              'features[Math.floor(Math.random() * rooms.length)]',
-              'description',
-              'photos[]'},
-
-    'location': {'x = Math.floor(Math.random() * 10)',
-                 'y = Math.floor(Math.random() * 10)'}
-  },
-  {
-    'author': {'img/avatars/user08.png'},
-
-    'offer': {'title[7]',
-              'address: location.x, location.y',
-              'price[Math.random() * 1000]',
-              'type[3]',
-              'rooms[Math.floor(Math.random() * rooms.length)]',
-              'guests[Math.floor(Math.random() * 10)]',
-              'checkin[2]',
-              'checkout[1]',
-              'features[Math.floor(Math.random() * rooms.length)]',
-              'description', 'photos[]'},
-
-    'location': {'x = Math.floor(Math.random() * 10)',
-                 'y = Math.floor(Math.random() * 10)'}
+    location: {
+      x: Math.floor(Math.random() * 500),
+      y: Math.floor(Math.random() * 500)
+    }
   }
 ];
 
+console.log(usersAds);
 
-var turnMap = document.querySelector('.map');
-turnMap.classList.remove('map--faded');
+var turnMap = document.querySelector(".map");
+turnMap.classList.remove("map--faded");
 
-var template = document.querySelector('template').content.querySelector('.map__pin');
+var template = document
+  .querySelector("template")
+  .content.querySelector(".map__pin");
 
-var mapPins = document.querySelector('.map__pins');
+var mapPins = document.querySelector(".map__pins");
 
-for (var i = 0; i < usersAds.length; i++) {
+var renderMen = function(user) {
   var userElement = template.cloneNode(true);
 
-  userElement.querySelector('.map__pin').style.left = usersAds[i].location.x;
-  userElement.querySelector('.map__pin').style.top = usersAds[i].location.y;
+  userElement.style.left = usersAds[i].location.x + "px";
+  userElement.style.top = usersAds[i].location.y + "px";
 
+  userElement.querySelector("img").src = usersAds[i].author.avatar;
 
-  userElement.querySelector('img').src = usersAds[i].author;
+  userElement.querySelector("img").alt = usersAds[i].offer.title;
 
-  userElement.querySelector('img').alt = usersAds[i].title;
+  return userElement;
+};
 
-  mapPins.appendChild(userElement);
-
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < usersAds.length; i++) {
+  fragment.appendChild(renderMen(usersAds[i]));
 }
+
+mapPins.appendChild(fragment);
