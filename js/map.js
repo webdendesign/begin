d91bb644ee621d7dd46f8b7a6bbcc3719e081119
Den <fabri.din@yandex.ru>
2019-12-12 00:46:32 +0500

    merge

--- js/map.js
+++ js/map.js
@@ -44,8 +44,8 @@ var usersAds = [
       title: title[0],
       address: "location.x, location.y",
       price: Math.floor(Math.random() * 1000000),
-      type: type[0],
-      rooms: Math.floor(Math.random() * rooms.length),
+      type: type[2],
+      rooms: Math.round(Math.random() * rooms.length),
       guests: Math.floor(Math.random() * 10),
       checkin: checkin[0],
       checkout: checkout[1],
@@ -61,19 +61,23 @@ var usersAds = [
   }
 ];
 
-console.log(usersAds);
-
 var turnMap = document.querySelector(".map");
 turnMap.classList.remove("map--faded");
 
-var template = document
+var templatePin = document
   .querySelector("template")
   .content.querySelector(".map__pin");
 
 var mapPins = document.querySelector(".map__pins");
 
+var templateCard = document
+  .querySelector("template")
+  .content.querySelector(".map__card");
+
+var mapFiltersContainer = document.querySelector(".map__filters-container");
+
 var renderMen = function(user) {
-  var userElement = template.cloneNode(true);
+  var userElement = templatePin.cloneNode(true);
 
   userElement.style.left = usersAds[i].location.x + "px";
   userElement.style.top = usersAds[i].location.y + "px";
@@ -85,9 +89,65 @@ var renderMen = function(user) {
   return userElement;
 };
 
-var fragment = document.createDocumentFragment();
+var typeHouse = function(type) {
+  if (type === "palace") {
+    return "Дворец";
+  } else if (type === "flat") {
+    return "Квартира";
+  } else if (type === "bungalo") {
+    return "Бунгало";
+  } else return "Дом";
+};
+
+var renderAds = function(ads) {
+  var adsElement = templateCard.cloneNode(true);
+
+  adsElement.querySelector(".popup__title").textContent =
+    usersAds[i].offer.title;
+  adsElement.querySelector(".popup__text--address").textContent =
+    usersAds[i].offer.address;
+  adsElement.querySelector(".popup__text--price").textContent =
+    usersAds[i].offer.price + " /ночь";
+
+  adsElement.querySelector(".popup__type").textContent = typeHouse(
+    usersAds[i].offer.type
+  );
+
+  adsElement.querySelector(".popup__text--capacity").textContent =
+    usersAds[i].offer.rooms +
+    " комнаты для " +
+    usersAds[i].offer.guests +
+    " гостей";
+
+  adsElement.querySelector(".popup__text--time").textContent =
+    "Заезд после " +
+    usersAds[i].offer.checkin +
+    ", выезд до " +
+    usersAds[i].offer.checkout;
+
+  adsElement.querySelector(".popup__features").textContent =
+    usersAds[i].offer.features;
+
+  adsElement.querySelector(".popup__description").textContent =
+    usersAds[i].offer.description;
+
+  adsElement.querySelector(".popup__photos").src = usersAds[i].offer.photos;
+
+  adsElement.querySelector(".popup__avatar").src = usersAds[i].author.avatar;
+
+  return adsElement;
+};
+
+var fragmentPin = document.createDocumentFragment();
 for (var i = 0; i < usersAds.length; i++) {
-  fragment.appendChild(renderMen(usersAds[i]));
+  fragmentPin.appendChild(renderMen(usersAds[i]));
 }
 
-mapPins.appendChild(fragment);
+mapPins.appendChild(fragmentPin);
+
+var fragmentCard = document.createDocumentFragment();
+for (var i = 0; i < usersAds.length; i++) {
+  fragmentCard.appendChild(renderAds(usersAds[i]));
+}
+
+mapFiltersContainer.appendChild(fragmentCard);
