431015aff74e76afe7f5e6cad3b66d18fc5e7b93
Den <fabri.din@yandex.ru>
2019-12-15 16:38:56 +0500

    done event

--- js/map.js
+++ js/map.js
@@ -368,16 +368,44 @@ var form = document.querySelector(".ad-form");
 var type = form.querySelector("#type");
 var option = type.querySelectorAll("option");
 var price = form.querySelector("#price");
-console.log(option);
-console.log(price);
-for (var i = 0; i < option.length; i++) {
-  if (option[i].value == "bungalo") {
-    price.placeholder = "0";
-  } else if (option[i].value == "flat") {
-    price.placeholder = "1000";
-  } else if (option[i].value == "house") {
-    price.placeholder = "5000";
-  } else if (option[i].value == "palace") {
-    price.placeholder = "10000";
+// console.log(option);
+// console.log(price);
+var valuePlaceholder = function(option) {
+  if (option.value === "bungalo") {
+    return (price.placeholder = "0");
+  } else if (option.value === "flat") {
+    return (price.placeholder = "1000");
+  } else if (option.value === "house") {
+    return (price.placeholder = "5000");
+  } else if (option.value === "palace") {
+    return (price.placeholder = "10000");
   }
+};
+
+for (var i = 0; i < option.length; i++) {
+  valuePlaceholder(option[i]);
 }
+
+// var timein = document.querySelector("#timein");
+// var timeout = document.querySelector("#timeout");
+// var optionTimein = timein.querySelectorAll("option");
+// var optionTimeout = timeout.querySelectorAll("option");
+
+// var choiceTime = function(timein) {
+//   if (timein === "После 12") {
+//     return (timeout.textContent = "Выезд до 12");
+//   } else if (timein === "После 13") {
+//     return (timeout.textContent = "Выезд до 13");
+//   } else if (timein === "После 14") {
+//     return (timeout.textContent = "Выезд до 14");
+//   }
+// };
+
+// for (var i = 0; i < optionTimein.length; i++) {
+//   choiceTime(optionTimein[i].textContent);
+// }
+
+document.querySelector("form").onchange = function(evt) {
+  this.timein.value = evt.target.value;
+  this.timeout.value = evt.target.value;
+};
